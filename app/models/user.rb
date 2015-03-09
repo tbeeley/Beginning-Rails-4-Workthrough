f1cd3795ab8a6a560502e87699606715e0b504b3
Thomas Beeley <tbeeley@gmail.com>
2015-03-09 16:48:06 +0000

    End of Chap6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
+
   
   validates_uniqueness_of :email
   validates_length_of :email, :within => 5..50
-  validates_format_of :email, :with =>  /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i
+  # validates_format_of :email, :with =>  /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i
+  # This is causing an error in the terminal. Something to do with Javascript injection worry. 
   validates_confirmation_of :password
   validates_length_of :password, :within => 4..20
   validates_presence_of :password, :if => :password_required?
